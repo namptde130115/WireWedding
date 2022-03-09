ee34684d5019e085e8f327ec7ab46e3bc33490e7
LuuPhuongUyen <luuphuonguyen1485@gmail.com>
2022-03-09 20:54:26 +0700

    fix conflict

--- src/pages/homepage/index.jsx
+++ src/pages/homepage/index.jsx
@@ -1,12 +1,7 @@
-<<<<<<< HEAD
-// import { ButtonCustom } from "../../components/ButtonCustom/index.jsx";
-import { HomeLayout } from "../../layout/homepage/index.jsx";
-import { HomepageContent } from "../../components/HomepageContent/index.jsx";
-=======
 import { ButtonCustom } from '../../components/ButtonCustom/index.jsx';
 import { CardInfor } from '../../components/CardInfor/index.jsx';
 import { HomeLayout } from '../../layout/homepage/index.jsx';
->>>>>>> 330b0961327440f2bd615b995fff677661795ce7
+import { HomepageContent } from '../../components/HomepageContent/index.jsx';
 
 export const HomePage = () => {
   return (
