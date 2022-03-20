f81f5658545ff475f8844949793453028c83393e
ShinCN <shincnielts@gmail.com>
2022-03-20 16:21:52 +0700

    hung commit

--- src/App.js
+++ src/App.js
@@ -8,14 +8,21 @@ import { SignUp } from './pages/signup';
 import { Counter } from './features/counter/Counter';
 import { HomePage } from './pages/homepage';
 import { ProfilePage } from './pages/profile';
-
+import { PlanningTools } from './pages/planningtools/planningtools_home';
+import { Added_Service_Manage } from './pages/planningtools/added_services_management';
+import { Hired_Service_Manage } from './pages/planningtools/hired_services_manangement';
+import { Budget } from './pages/planningtools/budget';
 function App() {
   return (
     <div>
       {/* <SignIn /> */}
       {/* <SignUp /> */}
-      <HomePage />
+      {/* <HomePage /> */}
       {/* <ProfilePage /> */}
+      <PlanningTools />
+      {/* <Added_Service_Manage /> */}
+      {/* <Hired_Service_Manage /> */}
+      {/* <Budget/> */}
     </div>
   );
 }
