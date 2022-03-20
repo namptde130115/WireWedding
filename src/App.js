2658d50750d75199238de4d572c3a6cd30770e5f
trung nam <cfpro99vn@gmail.com>
2022-03-20 12:58:15 +0700

    push code

--- src/App.js
+++ src/App.js
@@ -14,8 +14,8 @@ function App() {
     <div>
       {/* <SignIn /> */}
       {/* <SignUp /> */}
-      <HomePage />
-      {/* <ProfilePage /> */}
+      {/* <HomePage /> */}
+      <ProfilePage />
     </div>
   );
 }
--- src/pages/profile/index.jsx
+++ src/pages/profile/index.jsx
@@ -1,9 +1,24 @@
 import { ProfileLayout } from '../../layout/profile';
+import { PaymentInfor } from '../payment-infor';
+import { CheckList } from '../planning-tools-nam';
+import { FavoriteProfile } from './favorites/index';
+import { InforProfile } from './infor/index';
+import { Notification } from './notification';
+import { Payment } from './payment-history';
 
 export const ProfilePage = () => {
   return (
     <ProfileLayout title='Profile'>
-      <div>sssss</div>
+      {/* <InforProfile /> */}
+      <FavoriteProfile />
+      <Notification
+        title='lorem Ipsum kasjdalksjdlakjdqiwjd'
+        day='09/02/2021'
+        time='10:00'
+      />
+      <Payment />
+      <PaymentInfor />
+      <CheckList />
     </ProfileLayout>
   );
 };
