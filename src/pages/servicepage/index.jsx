42a3bb3dde63fd81d53edab3d024e2a8b81001a6
trung nam <cfpro99vn@gmail.com>
2022-03-20 16:35:27 +0700

    Merge branch 'develop' of https://github.com/namptde130115/WireWedding into profilepage

--- src/pages/servicepage/index.jsx
+++ src/pages/servicepage/index.jsx
@@ -7,7 +7,6 @@ import { Paging } from '../../components/Pagination/index.jsx';
 import styles from './index.module.scss';
 
 export const Service = () => {
-<<<<<<< HEAD
     return (
         <ServiceLayout>
             <div className={styles.utility}>
@@ -64,96 +63,3 @@ export const Service = () => {
         </ServiceLayout>
     );
 }
-=======
-  return (
-    <ServiceLayout>
-      <div className={styles.utility}>
-        <SearchBar />
-        <Category />
-        <Location />
-      </div>
-      <div className={styles.card}>
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='chup anh'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='anh chup'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='hna puhc'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='puhc hna'
-          rate='5'
-          location='Da Nang'
-        />
-      </div>
-      <div className={styles.card}>
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='chup anh'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='anh chup'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='hna puhc'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='puhc hna'
-          rate='5'
-          location='Da Nang'
-        />
-      </div>
-      <div className={styles.card}>
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='chup anh'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='anh chup'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='hna puhc'
-          rate='5'
-          location='Da Nang'
-        />
-        <CardInfor
-          card_img='https://firebasestorage.googleapis.com/v0/b/gotobun-260222.appspot.com/o/Service%2Fstudio_01.PNG?alt=media&token=1d0cfe71-8e3a-48ad-860e-6e0f0e816368'
-          title='puhc hna'
-          rate='5'
-          location='Da Nang'
-        />
-      </div>
-      <div className={styles.paging}>
-        <Paging />
-      </div>
-    </ServiceLayout>
-  );
-};
->>>>>>> 0ea75ffccb47b78595a8bed380defdc98fc00f97
