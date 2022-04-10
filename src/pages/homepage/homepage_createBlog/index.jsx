9373f7c69eb195acf3a4d224651d0ccf0f08b299
trung nam <namptde130115@fpt.edu.vn>
2022-04-10 15:33:06 +0700

    Handle catch post content

--- src/pages/homepage/homepage_createBlog/index.jsx
+++ src/pages/homepage/homepage_createBlog/index.jsx
@@ -1,55 +1,75 @@
-import React, { useRef } from "react";
-import { Editor } from "@tinymce/tinymce-react";
-import clsx from "clsx";
-import styles from "./index.module.scss";
-import { Input } from "antd";
-import { ButtonCustom } from "../../../components/ButtonCustom/index.jsx";
+import React, { useRef, useState } from 'react';
+import { Editor } from '@tinymce/tinymce-react';
+import clsx from 'clsx';
+import styles from './index.module.scss';
+import { Input } from 'antd';
+import { ButtonCustom } from '../../../components/ButtonCustom/index.jsx';
 
 export const HomePageCreateBlog = () => {
   const editorRef = useRef(null);
-  const log = () => {
-    if (editorRef.current) {
-      console.log(editorRef.current.getContent());
-    }
+  const [initContent] = useState({
+    postHeading: 'Blog Title',
+    postBodyContent: '<p>This is the initial content.</p>'
+  });
+  const [postContent, setPostContent] = useState({
+    postHeading: '',
+    postBodyContent: ''
+  });
+
+  const getContent = () => {
+    console.log(postContent);
   };
+
+  const handleEditorChange = (value) => {
+    setPostContent({ ...postContent, postBodyContent: value });
+  };
+
+  const handleTitleChange = (e) => {
+    setPostContent({ ...postContent, postHeading: e.target.value });
+  };
+
   return (
     <div className={clsx(styles.createBlog_container)}>
-      <Input placeholder="Blog Title" bordered={false} />
+      <Input
+        onChange={handleTitleChange}
+        placeholder={initContent.postHeading}
+        bordered={false}
+      />
       <div className={clsx(styles.createBlog_child)}>
         <Editor
-          apiKey="cl0i398qnef0mj3s5d5a1r8syteuq89aj2dzjw9xvcjjlyt5"
+          apiKey='cl0i398qnef0mj3s5d5a1r8syteuq89aj2dzjw9xvcjjlyt5'
           onInit={(evt, editor) => {
             editorRef.current = editor;
           }}
-          initialValue="<p>This is the initial content of the editor.</p>"
+          initialValue={initContent.postBodyContent}
           init={{
             height: 700,
             menubar: true,
             /* enable title field in the Image dialog*/
             image_title: true,
             plugins: [
-              "advlist autolink lists link image charmap print preview anchor",
-              "searchreplace visualblocks code fullscreen",
-              "insertdatetime media table paste image code help wordcount",
+              'advlist autolink lists link image charmap print preview anchor',
+              'searchreplace visualblocks code fullscreen',
+              'insertdatetime media table paste image code help wordcount'
             ],
             toolbar:
-              "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect " +
-              "| alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist " +
-              "| forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons " +
-              "| fullscreen  preview save print | insertfile image media pageembed template link anchor codesample" +
-              "| a11ycheck ltr rtl | showcomments addcomment",
+              'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect ' +
+              '| alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist ' +
+              '| forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons ' +
+              '| fullscreen  preview save print | insertfile image media pageembed template link anchor codesample' +
+              '| a11ycheck ltr rtl | showcomments addcomment',
             file_picker_callback: function (cb, value, meta) {
-              var input = document.createElement("input");
-              input.setAttribute("type", "file");
-              input.setAttribute("accept", "image/*");
+              var input = document.createElement('input');
+              input.setAttribute('type', 'file');
+              input.setAttribute('accept', 'image/*');
               input.onchange = function () {
                 var file = this.files[0];
 
                 var reader = new FileReader();
                 reader.onload = function () {
-                  var id = "blobid" + new Date().getTime();
+                  var id = 'blobid' + new Date().getTime();
                   var blobCache = editorRef.current.editorUpload.blobCache;
-                  var base64 = reader.result.split(",")[1];
+                  var base64 = reader.result.split(',')[1];
                   var blobInfo = blobCache.create(id, file, base64);
                   blobCache.add(blobInfo);
 
@@ -61,14 +81,15 @@ export const HomePageCreateBlog = () => {
               input.click();
             },
             content_style:
-              "@import url('https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap'); body { font-family: 'Lora', serif; font-size:14pt }",
+              "@import url('https://fonts.googleapis.com/css2?family=Lora:ital@1&display=swap'); body { font-family: 'Lora', serif; font-size:14pt }"
           }}
+          onEditorChange={handleEditorChange}
         />
         {/* <button onClick={log}>Log editor content</button> */}
       </div>
       <div className={clsx(styles.button_container)}>
-        <ButtonCustom type="primary" text="Create" />
-        <ButtonCustom type="ghost" text="Cancel" />
+        <ButtonCustom onClick={getContent} type='primary' text='Create' />
+        <ButtonCustom type='ghost' text='Cancel' />
       </div>
     </div>
   );
